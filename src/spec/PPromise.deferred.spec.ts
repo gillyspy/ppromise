1396af7a2824fa143a98775ba46e502693c13bdf
gillyspy <gillyspy@gmail.com>
2021-06-11 12:11:22 -0400

    default type is standard type

--- src/spec/PPromise.deferred.spec.ts
+++ src/spec/PPromise.deferred.spec.ts
@@ -3,14 +3,17 @@ import {ppTypes, optionsType} from '../Types'
 import IllegalOperationError from "../errors/IllegalOperationError";
 
 describe( 'Deferred Pattern', ()=>{
-    test('instantiate PPromise as type fluid will make it a fluid:deception type',()=> {
+    test('instantiate PPromise as type fluid will make it a standard:deception type',()=> {
         const myPPromise = new PPromise({
             type : ppTypes.STANDARD
         });
         expect(myPPromise.type).toEqual(ppTypes.STANDARD);
     });
 
-    test('')
+    test('default type is standard', ()=>{
+        const myPPromise = new PPromise();
+        expect(myPPromise.type).toEqual(ppTypes.STANDARD);
+    })
 
         /*with a string will establish that string as resolve value', ()=>{
         const myPPromise = new PPromise('lonely string');
