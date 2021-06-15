69e81e71f0cc6e29834308affb2370a5abd8b6d0
gillyspy <gillyspy@gmail.com>
2021-06-15 01:59:47 -0400

    ran a build

--- dist/PPromise.js
+++ dist/PPromise.js
@@ -6,24 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Types_1 = require("./Types");
 const IllegalOperationError_1 = __importDefault(require("./errors/IllegalOperationError"));
 const InvalidDefinitionError_1 = __importDefault(require("./errors/InvalidDefinitionError"));
+const Registry = {};
 class PPromise {
     //constructor(values: callbackArgs, options?: object );
     //constructor(options?: object);
     //constructor(callback: Function, value: any, options?: object );
     //constructor(promise: Promise<any>, value: any, options?: object);
     constructor(...args) {
+        var _a, _b;
+        this.name = Symbol(Math.random().toString());
         this._type = Types_1.ppTypes.FLUID;
-        this._secret = Symbol('secret');
         this._isFulfilled = false;
         this._isPending = true;
         this._isRejected = false;
         this._isResolved = false;
+        this._chainSecret = Symbol('chain');
         this._isTriggered = false;
-        this._isUnbreakable = true;
         this.options = {
-            name: this.name,
-            isUnbreakable: this._isUnbreakable,
-            type: Types_1.ppTypes.FLUID
+            name: Symbol(Math.random().toString()),
+            isUnbreakable: true,
+            type: Types_1.ppTypes.FLUID,
+            secret: undefined,
+            registry: Registry
         };
         if (args.length > 2)
             throw new InvalidDefinitionError_1.default('constructor');
@@ -32,20 +36,17 @@ class PPromise {
             //options has to be the second one
             this.options = this.parseOptions(args.pop());
         //args.length === 1
-        if (args.length === 1 && !Array.isArray(args[0]) && typeof args[0] === 'object')
+        if (args.length === 1 && !Array.isArray(args[0]) && typeof args[0] === 'object' && !(args[0] instanceof Promise))
             this.options = this.parseOptions(args.pop());
         //args.length = 0
         const that = this;
-        let callback;
+        let callback, registry, promise;
         //apply options
         if (typeof this.options.type !== 'undefined')
             this._type = this.options.type;
-        if (typeof this.options.isUnbreakable !== 'undefined')
-            this._isUnbreakable = this.options.isUnbreakable;
-        if (typeof this.options.name !== 'undefined')
-            this.name = this.options.name;
-        if (this._type === Types_1.ppTypes.GAS)
-            this._isUnbreakable = false;
+        this.name = ((_a = this.options) === null || _a === void 0 ? void 0 : _a.name) || Symbol('unknown');
+        this._secret = (_b = this.options) === null || _b === void 0 ? void 0 : _b.secret;
+        registry = this.options.registry || Registry;
         //init states
         this.initStates();
         //Build Promise
@@ -54,33 +55,67 @@ class PPromise {
             if (cbOrPromiseOrValues instanceof Promise)
                 if (this._type === Types_1.ppTypes.SOLID) {
                     // Promises for SOLID can be substituted straight in more-or-less
-                    this._promise = cbOrPromiseOrValues;
+                    promise = cbOrPromiseOrValues;
                 }
                 else {
                     //TODO: deal with promises
+                    throw new InvalidDefinitionError_1.default('Native Promise', `They are only currently supported for ${Types_1.ppTypes.SOLID}. Please use Promise.all, any, etc instead`);
                 }
             if (typeof cbOrPromiseOrValues === 'function') {
-                callback = cbOrPromiseOrValues;
+                const cb = cbOrPromiseOrValues;
+                if (this._type === Types_1.ppTypes.SOLID)
+                    callback = cbOrPromiseOrValues;
+                else
+                    callback = (nativeResolve, nativeReject) => {
+                        let calledOnce = false;
+                        if (calledOnce) {
+                            console.log('subsequent call to resolve ignored.');
+                            return;
+                        }
+                        this._nativeResolveProxy = (deferredValue, deferredMsg) => {
+                            calledOnce = true;
+                            new Promise((innerResolve, innerReject) => {
+                                cb(innerResolve, innerReject, deferredValue, deferredMsg);
+                            }).then(nativeResolve, nativeReject);
+                        };
+                        this._nativeRejectProxy = (deferredMsg, deferredValue) => {
+                            calledOnce = true;
+                            new Promise((innerResolve, innerReject) => {
+                                cb(innerResolve, innerReject, deferredValue, deferredMsg);
+                            }).then(nativeResolve, nativeReject);
+                        };
+                    };
             }
             if (Array.isArray(cbOrPromiseOrValues)) {
-                this._value = cbOrPromiseOrValues[0];
-                this._reason = cbOrPromiseOrValues[1];
-                callback = callback;
+                let [v, r] = cbOrPromiseOrValues;
+                if (typeof v === 'function')
+                    this._resolveCallback = v;
+                else
+                    this._value = v;
+                if (typeof r === 'function')
+                    this._rejectCallback = r;
+                else
+                    this._reason = r;
                 //TODO: seems pointless to pass in a static reject value for a SOLID
             }
         }
         if (!callback)
             callback = (resolve, reject) => {
-                that._nativeResolve = (v) => {
+                that._nativeResolveProxy = (v) => {
                     resolve(v);
                 };
-                that._nativeReject = reject;
+                that._nativeRejectProxy = reject;
             };
-        this._promise = PPromise.createNativePromise(callback.bind(this));
+        if (typeof promise === 'undefined')
+            promise = PPromise.createNativePromise(callback.bind(this));
+        this._promise = promise;
         if (this._type === Types_1.ppTypes.SOLID)
             this._resolve(this._value);
-        if (this._type === Types_1.ppTypes.GAS && !this.isUnbreakable)
+        if (this._type === Types_1.ppTypes.GAS)
             this.createChain();
+        //TODO: https://github.com/microsoft/TypeScript/issues/1863
+        if (!/^symbol.chain/i.test(this.name.toString()))
+            registry[this.name.toString()] = this;
     }
     parseOptions(options) {
         if (typeof options === 'undefined')
@@ -92,15 +127,31 @@ class PPromise {
             throw new InvalidDefinitionError_1.default('isUnbreakable', `Must be ${!this.isUnbreakable} for type ${options.type}`);
         return options;
     }
+    static checkPermissions(key, secret) {
+        let keyType = typeof key;
+        if (keyType === 'undefined' || keyType !== typeof secret)
+            throw new TypeError(`Key provided is type ${keyType} and not in the correct format of ${typeof secret}`);
+        if (keyType !== 'string' && keyType !== 'symbol')
+            throw new TypeError(`Key provided is type ${keyType} and not in the correct format of ${typeof secret}`);
+        if (secret !== key && typeof key !== 'undefined')
+            throw new IllegalOperationError_1.default(`Key ${key.toString()} does not match secret`);
+    }
+    isValidKey(candidateKey) {
+        if (!this.isSecured)
+            return true;
+        if (typeof candidateKey === 'undefined')
+            return false;
+        return candidateKey === this._secret;
+    }
     createChain() {
         if (this.isUnbreakable)
             return;
-        const secret = this._secret;
-        this._Chain = new PPromise({
-            name: secret,
+        const opts = {
+            name: this._chainSecret,
             type: Types_1.ppTypes.FLUID,
-            isUnbreakable: true
-        });
+            secret: this._chainSecret
+        };
+        this._Chain = new PPromise(opts);
     }
     linkAnyChains() {
         if (!this._Chain)
@@ -125,6 +176,8 @@ class PPromise {
         return this._type;
     }
     get promise() {
+        //note: types change so... do not rely upon isUnbreakable
+        //if it has a chain then return it
         if (this.chain instanceof PPromise)
             return this.chain.promise;
         return this._promise; // also implies unbreakable
@@ -132,11 +185,17 @@ class PPromise {
     get isTriggered() {
         return this._isTriggered;
     }
+    get isSettled() {
+        return this.isFulfilled || this.isRejected;
+    }
     get isFulfilled() {
         return this._isFulfilled;
     }
     get isPending() {
         return this._isPending;
+    }
+    get isSecured() {
+        return !!this._secret;
     }
     get isRejected() {
         return this._isRejected;
@@ -145,64 +204,80 @@ class PPromise {
         return this._isResolved;
     }
     get isUnbreakable() {
-        return this._isUnbreakable;
+        return (this._type === Types_1.ppTypes.SOLID || this._type === Types_1.ppTypes.FLUID);
+    }
+    resolveRejectPrep(values) {
+        const name = this.name;
+        if (this._type === Types_1.ppTypes.SOLID || this.isSettled || this.isTriggered)
+            console.log(`Promise (name: ${name.toString()} already Settled earlier with and is now ${this._type}`);
+        //throw new IllegalOperationError('resolve cannot be forced on ' + ppTypes.SOLID);
+        const key = this.isSecured ? values.pop() : undefined;
+        if (!this.isValidKey(key))
+            throw new IllegalOperationError_1.default(`This instance (${name.toString()} is secure. You must provide matching key as the last argument`);
+        return values;
     }
     static resolve(value) {
         return new PPromise([value], {
-            isUnbreakable: true,
             type: Types_1.ppTypes.SOLID
         });
     }
+    /* same as resolve except it returns current instance for object chaining */
+    pushResolve(...values) {
+        this.resolve(...values);
+        return this;
+    }
     resolve(...values) {
-        if (this._type === Types_1.ppTypes.SOLID)
-            throw new IllegalOperationError_1.default('resolve cannot be forced on ' + Types_1.ppTypes.SOLID);
+        values = this.resolveRejectPrep(values);
         if (values.length)
-            return this._resolve(values[0]);
-        return this._resolve();
+            this._resolve(values[0]);
+        else
+            this._resolve();
+        return this.promise;
     }
     _resolve(...values) {
         var _a;
         let callbackForThen, resolveValue;
-        if (!this.isResolved && !this.isTriggered) {
-            if (typeof this._value === 'function') {
-                callbackForThen = this.makeFulfillmentCallback(this._value);
-                resolveValue = values[0];
-            }
-            else {
-                this._value = values.length ? values[0] : this._value;
-                resolveValue = this._value;
-                callbackForThen = this.makeFulfillmentCallback();
-            }
-            this._isTriggered = true;
-            this._promise = this._promise.then(callbackForThen);
-            this.linkAnyChains();
-            //TODO: native resolve and then above is only needed if the promises was constructed from a callback
-            (_a = this._nativeResolve) === null || _a === void 0 ? void 0 : _a.call(this, resolveValue);
-        }
+        if (this.isSettled || this.isTriggered)
+            return this;
+        this._value = values.length ? values[0] : this._value;
+        resolveValue = this._value;
+        callbackForThen = this.makeFulfillmentCallback();
+        this._isTriggered = true;
+        this._promise = this._promise.then(callbackForThen);
+        this.linkAnyChains();
+        //TODO: native resolve and then above is only needed if the promises was constructed from a callback
+        (_a = this._nativeResolveProxy) === null || _a === void 0 ? void 0 : _a.call(this, resolveValue);
         return this;
     }
-    makeFulfillmentCallback(fn) {
+    makeFulfillmentCallback() {
         const that = this;
-        return (...v) => {
+        return (v) => {
+            let fn = that._resolveCallback;
             that._isFulfilled = true;
             that._isResolved = true;
             that._isPending = false;
             that._isRejected = false;
-            that._value = fn ? fn(v[0]) : v[0];
+            // update internal _value ( result )
+            that._value = fn ? fn(v) : v;
             return that._value;
         };
     }
-    reject(value) {
-        if (this._type === Types_1.ppTypes.SOLID)
-            throw new IllegalOperationError_1.default('reject cannot be forced on ' + Types_1.ppTypes.SOLID);
-        if (typeof value !== 'undefined')
-            return this._reject(value);
-        return this._reject();
+    pushReject(...values) {
+        this.reject(...values);
+        return this;
+    }
+    reject(...values) {
+        values = this.resolveRejectPrep(values);
+        const value = values[0];
+        if (typeof value === 'string' || value instanceof Error)
+            this._reject(value);
+        this._reject();
+        return this.promise;
     }
     _reject(...values) {
         var _a;
         let callbackForCatch, rejectValue;
-        if (!this.isRejected && !this.isTriggered) {
+        if (!this.isSettled && !this.isTriggered) {
             if (typeof this._reason === 'function') {
                 callbackForCatch = this.makeRejectCallback(this._reason);
                 rejectValue = values[0];
@@ -215,7 +290,7 @@ class PPromise {
             this._isTriggered = true;
             this._promise = this._promise.catch(callbackForCatch);
             this.linkAnyChains();
-            (_a = this._nativeReject) === null || _a === void 0 ? void 0 : _a.call(this, rejectValue);
+            (_a = this._nativeRejectProxy) === null || _a === void 0 ? void 0 : _a.call(this, rejectValue);
         }
         return this;
     }
@@ -235,11 +310,53 @@ class PPromise {
         this._isPending = true;
         this._isRejected = false;
         this._isResolved = false;
+        this._isTriggered = false;
     }
     static getDeferred(...args) {
-        //  return new PPromise(...args);
+        return new PPromise(...args);
     }
-    sever() {
+    static find(name, key) {
+        if (typeof name === 'undefined')
+            return;
+        //Symbols not supported as keys yet
+        //TODO: https://github.com/microsoft/TypeScript/issues/1863
+        let idx = name.toString();
+        const candidate = Registry[idx];
+        if (!candidate)
+            return;
+        if (!candidate.isSecured)
+            return candidate;
+        if (typeof key === 'undefined')
+            return;
+        if (candidate.isValidKey(key))
+            return candidate;
+        return;
+    }
+    sever(...args) {
+        if (this.isSecured) {
+            let key = args.pop();
+            PPromise.checkPermissions(key, this._secret);
+        }
+        let chainEvent = args[0];
+        if (chainEvent === 'resolve') {
+            try {
+                this.chain.resolve(this._chainSecret);
+            }
+            catch (e) {
+                console.log('chain failed to resolve', e.message);
+            }
+        }
+        else if (chainEvent === 'reject') {
+            try {
+                this.chain.reject(this._chainSecret);
+            }
+            catch (e) {
+                console.log('chain failed to resolve', e.message);
+            }
+        }
+        else if (typeof chainEvent !== 'undefined') {
+            throw new IllegalOperationError_1.default(`${chainEvent} is not a valid sever operation`);
+        }
         if (this.isUnbreakable)
             throw new IllegalOperationError_1.default(`You cannot sever a ${this.options.type}`);
         if (typeof this.chain !== 'undefined') {
@@ -248,24 +365,59 @@ class PPromise {
         }
         return this; //include case: not-unbreakable
     }
-    severSafely() {
-        try {
-            this.sever();
+    upgradeType(type, key) {
+        if (this.isSecured)
+            PPromise.checkPermissions(key, this._secret);
+        // no change
+        if (this._type === type)
+            return;
+        //impossible change
+        // SOLID to !SOLID
+        // !GAS to GAS
+        if (type === Types_1.ppTypes.GAS || this._type === Types_1.ppTypes.SOLID)
+            throw new IllegalOperationError_1.default(`Cannot downgrade ${this._type} to ${type}`);
+        //allowed change
+        if (type === Types_1.ppTypes.FLUID && this._type === Types_1.ppTypes.GAS) {
+            try {
+                //remove the chain
+                //--> don't think this is necessary
+                //continue to allow deferred
+                //set unbreakable = true
+                //change the type
+                this._type = Types_1.ppTypes.FLUID;
+            }
+            catch (e) {
+                console.log(e);
+                throw new IllegalOperationError_1.default(`upgrade from ${this._type} to ${type} failed. See log`);
+            }
         }
-        catch (e) {
-            console.log(e);
+        if (type === Types_1.ppTypes.SOLID) {
+            try {
+                //remove the chain
+                //--> don't think this is necessary
+                //disallow deferred
+                //resolve immediately
+                this._resolve();
+                //set unbreakable = true
+                //change the type
+                this._type = Types_1.ppTypes.SOLID;
+            }
+            catch (e) {
+                console.log(e);
+                throw new IllegalOperationError_1.default(`upgrade from ${this._type} to ${type} failed. See log`);
+            }
         }
-        return this;
     }
     pushThen(...args) {
         this.then(args);
         return this;
     }
     then(...args) {
-        console.log('then called by ', this.options.name);
-        if (this.isUnbreakable)
-            return this.promise.then(...args);
-        return this.chain.then(...args);
+        console.log('then called by ', this.name.toString());
+        return this.promise.then(...args);
+        /*
+        if (this.isUnbreakable) return this.promise.then(...args);
+        return this.chain.then(...args) */
     }
     catch(...args) {
         return this.promise.catch(...args);
--- dist/Types.d.ts
+++ dist/Types.d.ts
@@ -1,12 +1,19 @@
+import PPromise from "./PPromise";
+declare type registryType = {
+    [name: string]: PPromise;
+};
 declare enum ppTypes {
     'SOLID' = "solid:promise",
     'FLUID' = "fluid:deferred",
     'GAS' = "gas:deception"
 }
+declare type keyType = string | symbol;
 declare type optionsType = {
-    name?: string | symbol;
+    name?: keyType;
     isUnbreakable?: boolean;
     type?: ppTypes;
+    secret?: keyType;
+    registry?: registryType;
 };
 declare type resolveRejectArgs = [any, any?];
-export { ppTypes, optionsType, resolveRejectArgs };
+export { ppTypes, optionsType, resolveRejectArgs, keyType };
